03a77f5fc7bd97afb2414235ddb90acc483e3c69
Josué García <josueduardo.dev@gmail.com>
2025-09-05 07:29:34 -0600

    Init the project

---
+++ app/src/main/java/sv/edu/udb/guia8app/MainActivity.kt
@@ -0,0 +1,20 @@
+package sv.edu.udb.guia8app
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.database.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
